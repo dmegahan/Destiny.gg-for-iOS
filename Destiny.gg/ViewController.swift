dmegahan/Destiny.gg-for-iOS

commit 99e6a6fc76150b3482717497add3e964e73222df

Removed the constraint that attached the toolbar to the bottom of the status bar, it was causing an issue where the search box and lock frames button couldn't be interacted with. Reattached the search bar and button to their respective IBOutlets, which became disconnected when they were renamed to fit with the rest of the code style. Added the "semaphore.wait" phrases back in RestAPIManager functions, without them the functions weren't returning when finished.

Destiny.gg/Base.lproj/Main.storyboard
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="11201" systemVersion="15G1004" targetRuntime="iOS.CocoaTouch.iPad" propertyAccessControl="none" useAutolayout="YES" colorMatched="YES" initialViewController="BYZ-38-t0r">
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="11201" systemVersion="15G1108" targetRuntime="iOS.CocoaTouch.iPad" propertyAccessControl="none" useAutolayout="YES" colorMatched="YES" initialViewController="BYZ-38-t0r">
     <dependencies>
-        <deployment version="4096" identifier="iOS"/>
+        <deployment identifier="iOS"/>
         <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="11161"/>
         <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
     </dependencies>
@@ -15,34 +15,35 @@
                         <viewControllerLayoutGuide type="bottom" id="OnL-z6-xFJ"/>
                     </layoutGuides>
                     <view key="view" contentMode="scaleToFill" id="8bC-Xf-vdC">
-                        <rect key="frame" x="0.0" y="0.0" width="1024" height="1366"/>
+                        <rect key="frame" x="0.0" y="0.0" width="768" height="1024"/>
                         <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
                         <subviews>
                             <webView clipsSubviews="YES" contentMode="redraw" fixedFrame="YES" scalesPageToFit="YES" mediaPlaybackRequiresUserAction="NO" translatesAutoresizingMaskIntoConstraints="NO" id="fPX-Ha-yd1">
-                                <frame key="frameInset" minX="21" height="48.82%" maxX="6" maxY="16"/>
+                                <frame key="frameInset" minX="20" height="48.81%" maxX="7" maxY="16"/>
                                 <autoresizingMask key="autoresizingMask" widthSizable="YES" flexibleMinY="YES" heightSizable="YES"/>
                                 <color key="backgroundColor" red="0.28958413004875183" green="0.31462949514389038" blue="0.32950475811958313" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
                             </webView>
                             <webView autoresizesSubviews="NO" clipsSubviews="YES" contentMode="redraw" fixedFrame="YES" scalesPageToFit="YES" translatesAutoresizingMaskIntoConstraints="NO" id="Pge-yQ-1Sh" userLabel="myWebView">
-                                <frame key="frameInset" minX="124" minY="191" height="52.85%" maxX="-138"/>
+                                <frame key="frameInset" minX="123" minY="191" height="52.70%" maxX="-137"/>
                                 <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES" flexibleMaxY="YES"/>
                                 <color key="backgroundColor" red="0.28958413004875183" green="0.31462949514389038" blue="0.32950475811958313" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
                                 <connections>
                                     <outlet property="delegate" destination="BYZ-38-t0r" id="iMY-4B-rqQ"/>
                                 </connections>
                             </webView>
-                            <toolbar opaque="NO" clipsSubviews="YES" clearsContextBeforeDrawing="NO" contentMode="scaleToFill" ambiguous="YES" misplaced="YES" barStyle="black" translucent="NO" translatesAutoresizingMaskIntoConstraints="NO" id="wbc-Yh-wzI">
-                                <frame key="frameInset" minX="-1" minY="20" height="44"/>
+                            <toolbar opaque="NO" clipsSubviews="YES" clearsContextBeforeDrawing="NO" contentMode="scaleToFill" fixedFrame="YES" translucent="NO" translatesAutoresizingMaskIntoConstraints="NO" id="wbc-Yh-wzI">
+                                <frame key="frameInset" minY="20" width="769" height="44"/>
+                                <autoresizingMask key="autoresizingMask" flexibleMaxX="YES" flexibleMaxY="YES"/>
                                 <color key="backgroundColor" cocoaTouchSystemColor="darkTextColor"/>
                                 <items>
                                     <barButtonItem width="150" style="plain" id="HTL-DH-58p">
                                         <searchBar key="customView" contentMode="redraw" barStyle="black" text="Destiny" placeholder="twitch stream" id="Ljx-bf-wNV">
-                                            <rect key="frame" x="16" y="0.0" width="150" height="44"/>
+                                            <rect key="frame" x="20" y="0.0" width="150" height="44"/>
                                             <autoresizingMask key="autoresizingMask" widthSizable="YES" flexibleMaxY="YES"/>
                                             <textInputTraits key="textInputTraits" autocorrectionType="no" spellCheckingType="no"/>
                                         </searchBar>
                                     </barButtonItem>
-                                    <barButtonItem title="LF" width="50" id="DCh-o9-EB7">
+                                    <barButtonItem title="Lock" width="50" id="DCh-o9-EB7">
                                         <connections>
                                             <action selector="buttonPressed:" destination="BYZ-38-t0r" id="TG9-PG-uba"/>
                                         </connections>
@@ -51,16 +52,15 @@
                             </toolbar>
                         </subviews>
                         <color key="backgroundColor" red="1" green="1" blue="1" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
-                        <constraints>
-                            <constraint firstItem="wbc-Yh-wzI" firstAttribute="top" secondItem="UcK-g5-NgH" secondAttribute="bottom" id="qQ1-Th-aAc"/>
-                        </constraints>
                     </view>
                     <connections>
                         <outlet property="LockFramesButton" destination="DCh-o9-EB7" id="ikP-DU-hbF"/>
                         <outlet property="TwitchSearchBar" destination="Ljx-bf-wNV" id="SYb-Aq-Cu1"/>
+                        <outlet property="lockFramesButton" destination="DCh-o9-EB7" id="6ZW-B7-grX"/>
                         <outlet property="myChatWebView" destination="fPX-Ha-yd1" id="2uc-vQ-KAn"/>
                         <outlet property="myStreamWebView" destination="Pge-yQ-1Sh" id="3go-Vt-Whz"/>
-                        <outlet property="myToolBar" destination="wbc-Yh-wzI" id="oWh-wJ-cT5"/>
+                        <outlet property="myToolBar" destination="wbc-Yh-wzI" id="xcQ-tX-OGA"/>
+                        <outlet property="twitchSearchBar" destination="Ljx-bf-wNV" id="VkD-Mc-qHi"/>
                     </connections>
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="dkx-z0-nzr" sceneMemberID="firstResponder"/>
@@ -71,6 +71,6 @@
     <simulatedMetricsContainer key="defaultSimulatedMetrics">
         <simulatedStatusBarMetrics key="statusBar"/>
         <simulatedOrientationMetrics key="orientation"/>
-        <simulatedScreenMetrics key="destination" type="ipad12_9.fullscreen"/>
+        <simulatedScreenMetrics key="destination"/>
     </simulatedMetricsContainer>
 </document>

Destiny.gg/ViewController.swift
@@ -279,12 +279,14 @@ class ViewController: UIViewController, UIWebViewDelegate, UISearchBarDelegate{
             if(isLocked){
                 //remove all recognized gestures (currently only a pan swipe is used, so effectively only removing that)
                 self.view.gestureRecognizers?.removeAll();
+                lockFramesButton.title = "Unlock";
             }else{
                 //if there are no current gesture recognizers (if there is then we shouldn't do anything)
                 if(self.view.gestureRecognizers != nil){
                     //recreate the original panswipe and add it back like we do when the view loads
                     let panSwipe = UIPanGestureRecognizer(target: self, action: #selector(ViewController.OnPanSwipe(_:)));
                     self.view.addGestureRecognizer(panSwipe);
+                    lockFramesButton.title = "Lock";
                     
                     myChatWebView.scrollView.panGestureRecognizer .require(toFail: panSwipe);
                     myStreamWebView.scrollView.panGestureRecognizer.require(toFail: panSwipe);
@@ -298,9 +300,10 @@ class ViewController: UIViewController, UIWebViewDelegate, UISearchBarDelegate{
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let searchText = searchBar.text;
+        let searchText = searchBar.text?.lowercased();
 
         if(RestAPIManager.sharedInstance.doesStreamExist(searchText!)){
+            print("Found");
             embedStream(searchText!);
         }
     }

Destiny.gg/RestAPIManager.swift
@@ -46,7 +46,7 @@ class RestAPIManager: NSObject {
             semaphore.signal();
         }
         //we wait forever, should put a 30 second timer here or something
-        //semaphore.wait(timeout: DispatchTime.distantFuture)
+        semaphore.wait(timeout: DispatchTime.distantFuture)
         return isOnline;
     }
     
@@ -77,7 +77,7 @@ class RestAPIManager: NSObject {
             semaphore.signal();
         }
         //we wait forever, should put a 30 second timer here or something
-        //semaphore.wait(timeout: DispatchTime.distantFuture)
+        semaphore.wait(timeout: DispatchTime.distantFuture)
         return doesExist;
     }
     

commit 2333200f4992d665e0ee385a49d5215c94befe05

Fixed some broken constraints. The toolbar now stays attached to the status bar and can be interacted with, something that was broken in commit 99e6a6f .

Destiny.gg/Base.lproj/Main.storyboard
@@ -31,9 +31,7 @@
                                     <outlet property="delegate" destination="BYZ-38-t0r" id="iMY-4B-rqQ"/>
                                 </connections>
                             </webView>
-                            <toolbar opaque="NO" clipsSubviews="YES" clearsContextBeforeDrawing="NO" contentMode="scaleToFill" fixedFrame="YES" translucent="NO" translatesAutoresizingMaskIntoConstraints="NO" id="wbc-Yh-wzI">
-                                <frame key="frameInset" minY="20" width="769" height="44"/>
-                                <autoresizingMask key="autoresizingMask" flexibleMaxX="YES" flexibleMaxY="YES"/>
+                            <toolbar opaque="NO" clipsSubviews="YES" clearsContextBeforeDrawing="NO" contentMode="scaleToFill" translucent="NO" translatesAutoresizingMaskIntoConstraints="NO" id="wbc-Yh-wzI">
                                 <color key="backgroundColor" cocoaTouchSystemColor="darkTextColor"/>
                                 <items>
                                     <barButtonItem width="150" style="plain" id="HTL-DH-58p">
@@ -52,6 +50,11 @@
                             </toolbar>
                         </subviews>
                         <color key="backgroundColor" red="1" green="1" blue="1" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
+                        <constraints>
+                            <constraint firstAttribute="trailing" secondItem="wbc-Yh-wzI" secondAttribute="trailing" id="I7t-c4-nk2"/>
+                            <constraint firstItem="wbc-Yh-wzI" firstAttribute="leading" secondItem="8bC-Xf-vdC" secondAttribute="leading" id="Y25-fe-5Hb"/>
+                            <constraint firstItem="wbc-Yh-wzI" firstAttribute="top" secondItem="UcK-g5-NgH" secondAttribute="bottom" id="koj-SX-bzY"/>
+                        </constraints>
                     </view>
                     <connections>
                         <outlet property="LockFramesButton" destination="DCh-o9-EB7" id="ikP-DU-hbF"/>

Destiny.gg/ViewController.swift
@@ -40,6 +40,8 @@ class ViewController: UIViewController, UIWebViewDelegate, UISearchBarDelegate{
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false;
         
+        myToolBar.barTintColor = UIColor.black;
+        
         self.initializeConstraints();
         
         self.twitchSearchBar.delegate = self;
@@ -49,7 +51,6 @@ class ViewController: UIViewController, UIWebViewDelegate, UISearchBarDelegate{
         
         let streamer = "Destiny";
         
-        //let streamOnline = RestAPIManager.sharedInstance.isStreamOnline(streamer);
         embedStream(streamer);
 
         let panSwipe = UIPanGestureRecognizer(target: self, action: #selector(ViewController.OnPanSwipe(_:)));